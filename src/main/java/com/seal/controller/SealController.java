7bbf72f3705e9cf98b83db3f70834148aad23578
admin <admin@163.com>
2019-09-24 23:18:18 +0800

    添加activemq支持
    笔记：
    	1.添加activemq支持jar包
    	2.在properties文件中添加activemq相关配置，如下
    		spring.activemq.broker-url=tcp://localhost:61616
    		spring.activemq.in-memory=true
    		spring.activemq.pool.enabled=false
    		spring.activemq.user=admin
    		spring.activemq.password=admin
    		spring.activemq.packages.trust-all=true
    	3.创建发送消息的类，spring boot会自动生成JmsMessagingTemplate，利用该类来发送消息。
    	4.创建消息消费类，用JmsListener来标注处理消息的方法，方法的参数为发送消息时的参数

--- src/main/java/com/seal/controller/SealController.java
+++ src/main/java/com/seal/controller/SealController.java
@@ -3,17 +3,19 @@ package com.seal.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.seal.service.MailService;
+import com.seal.activemq.Producer;
+import com.seal.model.MailModel;
 
 @Controller
 public class SealController {
 	
 	@Autowired
-	private MailService mailService;
+	private Producer producer;
 	
 	@RequestMapping("/login")
 	public String login() {
@@ -23,9 +25,18 @@ public class SealController {
 	@RequestMapping("/home")
 	public String home() {
 //		mailService.sendMail("zhao", "this is a simple message", "zhaojiahong@zhaojiahong.com");
+		// mailService.sendMail("welcome", map, "zhaojiahong@zhaojiahong.com");
 		Map<String, Object> map = new HashMap<>();
 		map.put("username", "zhaojiahong");
-		mailService.sendMail("welcome", map, "zhaojiahong@zhaojiahong.com");
+		MailModel mailModel = new MailModel();
+		mailModel.setSubject("欢迎邮件");
+		mailModel.setTemplate("welcome");
+		mailModel.setHtmlText(true);
+		mailModel.setTemplateText(true);
+		mailModel.setToEmails(new String[] {"zhaojiahong@zhaojiahong.com", "lilinlin@zhaojiahong.com"});
+		mailModel.setParams(map);
+		producer.sendMessage(new ActiveMQQueue("seal/mail"), mailModel);
+		
 		return "home";
 	}
 }
